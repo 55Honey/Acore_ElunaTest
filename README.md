c3f33504134aca1c20dd1a8873c32347a8d1581b
55Honey <71938210+55Honey@users.noreply.github.com>
2021-10-29 22:25:18 +0200

    feat: testsuite for playerhooks

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# acore_ElunaTest
+## lua-eluna-test
  
+Lua script for Azerothcore with ElunaLUA to test hooks.
+
+#### Find me on patreon: https://www.patreon.com/Honeys
+
+
+## Requirements:
+Compile your [Azerothcore](https://github.com/azerothcore/azerothcore-wotlk) with [Eluna Lua](https://www.azerothcore.org/catalogue-details.html?id=131435473).
+The ElunaLua module itself usually doesn't require much setup/config. Just specify the subfolder where to put your lua_scripts in its .conf file.
+
+If the directory was not changed in the ElunaLua config, add the .lua script to your `../lua_scripts/` directory as a subfolder of the worldserver.
+
+
+## Admin Usage:
+
+Commands:
+
+`luatest` to list all events which weren't tested in this session.
+`resetluatest` to start an new session.
+
+
+## Currently listed events:
+
+PLAYER_EVENT_ON_CHARACTER_CREATE = 1        --(event, player)
+PLAYER_EVENT_ON_CHARACTER_DELETE = 2        --(event, guid)
+PLAYER_EVENT_ON_LOGIN = 3                   --(event, player)
+PLAYER_EVENT_ON_LOGOUT = 4                  --(event, player)
+PLAYER_EVENT_ON_SPELL_CAST = 5              --(event, player, spell, skipCheck)
+PLAYER_EVENT_ON_KILL_PLAYER = 6             --(event, killer, killed)
+PLAYER_EVENT_ON_KILL_CREATURE = 7           --(event, killer, killed)
+PLAYER_EVENT_ON_KILLED_BY_CREATURE = 8      --(event, killer, killed)
+PLAYER_EVENT_ON_DUEL_REQUEST = 9            --(event, target, challenger)
+PLAYER_EVENT_ON_DUEL_START = 10             --(event, player1, player2)
+PLAYER_EVENT_ON_DUEL_END = 11               --(event, winner, loser, type)
+PLAYER_EVENT_ON_GIVE_XP = 12                --(event, player, amount, victim) - Can return new XP amount
+PLAYER_EVENT_ON_LEVEL_CHANGE = 13           --(event, player, oldLevel)
+PLAYER_EVENT_ON_MONEY_CHANGE = 14           --(event, player, amount) - Can return new money amount
+PLAYER_EVENT_ON_REPUTATION_CHANGE = 15      --(event, player, factionId, standing, incremental) - Can return new standing
+PLAYER_EVENT_ON_TALENTS_CHANGE = 16         --(event, player, points)
+PLAYER_EVENT_ON_TALENTS_RESET = 17          --(event, player, noCost)
+PLAYER_EVENT_ON_CHAT = 18                   --(event, player, msg, Type, lang) - Can return false, newMessage
+PLAYER_EVENT_ON_WHISPER = 19                --(event, player, msg, Type, lang, receiver) - Can return false, newMessage
+PLAYER_EVENT_ON_GROUP_CHAT = 20             --(event, player, msg, Type, lang, group) - Can return false, newMessage
+PLAYER_EVENT_ON_GUILD_CHAT = 21             --(event, player, msg, Type, lang, guild) - Can return false, newMessage
+PLAYER_EVENT_ON_CHANNEL_CHAT = 22           --(event, player, msg, Type, lang, channel) - Can return false, newMessage
+PLAYER_EVENT_ON_EMOTE = 23                  --(event, player, emote) - Not triggered on any known emote
+PLAYER_EVENT_ON_TEXT_EMOTE = 24             --(event, player, textEmote, emoteNum, guid)
+PLAYER_EVENT_ON_SAVE = 25                   --(event, player)
+PLAYER_EVENT_ON_BIND_TO_INSTANCE = 26       --(event, player, difficulty, mapid, permanent)
+PLAYER_EVENT_ON_UPDATE_ZONE = 27            --(event, player, newZone, newArea)
+PLAYER_EVENT_ON_MAP_CHANGE = 28             --(event, player)
+-- Custom
+PLAYER_EVENT_ON_EQUIP = 29                  --(event, player, item, bag, slot)
+PLAYER_EVENT_ON_FIRST_LOGIN = 30            --(event, player)
+PLAYER_EVENT_ON_CAN_USE_ITEM = 31           --(event, player, itemEntry) - Can return InventoryResult enum value
+PLAYER_EVENT_ON_LOOT_ITEM = 32              --(event, player, item, count)
+PLAYER_EVENT_ON_ENTER_COMBAT = 33           --(event, player, enemy)
+PLAYER_EVENT_ON_LEAVE_COMBAT = 34           --(event, player)
+PLAYER_EVENT_ON_REPOP = 35                  --(event, player)
+PLAYER_EVENT_ON_RESURRECT = 36              --(event, player)
+PLAYER_EVENT_ON_LOOT_MONEY = 37             --(event, player, amount)
+PLAYER_EVENT_ON_QUEST_ABANDON = 38          --(event, player, questId)
+PLAYER_EVENT_ON_LEARN_TALENTS = 39          --(event, player, talentId, talentRank, spellid)
+-- UNUSED                     = 40          --(event, player)
+-- UNUSED                     = 41          --(event, player)
+PLAYER_EVENT_ON_COMMAND = 42                --(event, player, command) - player is nil if command used from console. Can return false
+PLAYER_EVENT_ON_PET_SPAWNED = 43            --(event, player, pet)
---
+++ elunatest.lua
@@ -0,0 +1,354 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Silvia
+-- Date: 29/10/2021
+-- Time: 20:36
+-- To change this template use File | Settings | File Templates.
+-- Originally created by Honey for Azerothcore
+-- requires ElunaLua module
+
+
+-- Test Eluna hooks in Azerothcore
+------------------------------------------------------------------------------------------------
+-- ADMIN GUIDE:  -  compile the core with ElunaLua module
+--               -  add this script to ../lua_scripts/
+------------------------------------------------------------------------------------------------
+
+
+PLAYER_EVENT_ON_CHARACTER_CREATE = 1        --(event, player)
+PLAYER_EVENT_ON_CHARACTER_DELETE = 2        --(event, guid)
+PLAYER_EVENT_ON_LOGIN = 3                   --(event, player)
+PLAYER_EVENT_ON_LOGOUT = 4                  --(event, player)
+PLAYER_EVENT_ON_SPELL_CAST = 5              --(event, player, spell, skipCheck)
+PLAYER_EVENT_ON_KILL_PLAYER = 6             --(event, killer, killed)
+PLAYER_EVENT_ON_KILL_CREATURE = 7           --(event, killer, killed)
+PLAYER_EVENT_ON_KILLED_BY_CREATURE = 8      --(event, killer, killed)
+PLAYER_EVENT_ON_DUEL_REQUEST = 9            --(event, target, challenger)
+PLAYER_EVENT_ON_DUEL_START = 10             --(event, player1, player2)
+PLAYER_EVENT_ON_DUEL_END = 11               --(event, winner, loser, type)
+PLAYER_EVENT_ON_GIVE_XP = 12                --(event, player, amount, victim) - Can return new XP amount
+PLAYER_EVENT_ON_LEVEL_CHANGE = 13           --(event, player, oldLevel)
+PLAYER_EVENT_ON_MONEY_CHANGE = 14           --(event, player, amount) - Can return new money amount
+PLAYER_EVENT_ON_REPUTATION_CHANGE = 15      --(event, player, factionId, standing, incremental) - Can return new standing
+PLAYER_EVENT_ON_TALENTS_CHANGE = 16         --(event, player, points)
+PLAYER_EVENT_ON_TALENTS_RESET = 17          --(event, player, noCost)
+PLAYER_EVENT_ON_CHAT = 18                   --(event, player, msg, Type, lang) - Can return false, newMessage
+PLAYER_EVENT_ON_WHISPER = 19                --(event, player, msg, Type, lang, receiver) - Can return false, newMessage
+PLAYER_EVENT_ON_GROUP_CHAT = 20             --(event, player, msg, Type, lang, group) - Can return false, newMessage
+PLAYER_EVENT_ON_GUILD_CHAT = 21             --(event, player, msg, Type, lang, guild) - Can return false, newMessage
+PLAYER_EVENT_ON_CHANNEL_CHAT = 22           --(event, player, msg, Type, lang, channel) - Can return false, newMessage
+PLAYER_EVENT_ON_EMOTE = 23                  --(event, player, emote) - Not triggered on any known emote
+PLAYER_EVENT_ON_TEXT_EMOTE = 24             --(event, player, textEmote, emoteNum, guid)
+PLAYER_EVENT_ON_SAVE = 25                   --(event, player)
+PLAYER_EVENT_ON_BIND_TO_INSTANCE = 26       --(event, player, difficulty, mapid, permanent)
+PLAYER_EVENT_ON_UPDATE_ZONE = 27            --(event, player, newZone, newArea)
+PLAYER_EVENT_ON_MAP_CHANGE = 28             --(event, player)
+-- Custom
+PLAYER_EVENT_ON_EQUIP = 29                  --(event, player, item, bag, slot)
+PLAYER_EVENT_ON_FIRST_LOGIN = 30            --(event, player)
+PLAYER_EVENT_ON_CAN_USE_ITEM = 31           --(event, player, itemEntry) - Can return InventoryResult enum value
+PLAYER_EVENT_ON_LOOT_ITEM = 32              --(event, player, item, count)
+PLAYER_EVENT_ON_ENTER_COMBAT = 33           --(event, player, enemy)
+PLAYER_EVENT_ON_LEAVE_COMBAT = 34           --(event, player)
+PLAYER_EVENT_ON_REPOP = 35                  --(event, player)
+PLAYER_EVENT_ON_RESURRECT = 36              --(event, player)
+PLAYER_EVENT_ON_LOOT_MONEY = 37             --(event, player, amount)
+PLAYER_EVENT_ON_QUEST_ABANDON = 38          --(event, player, questId)
+PLAYER_EVENT_ON_LEARN_TALENTS = 39          --(event, player, talentId, talentRank, spellid)
+-- UNUSED                     = 40          --(event, player)
+-- UNUSED                     = 41          --(event, player)
+PLAYER_EVENT_ON_COMMAND = 42                --(event, player, command) - player is nil if command used from console. Can return false
+PLAYER_EVENT_ON_PET_SPAWNED = 43            --(event, player, pet)
+
+
+local functionTested = {}
+local n
+
+--init all functions untested
+for n = 1,43,1 do
+    functionTested[n] = 0
+end
+--hardcoded marking as done what doesn't exist
+functionTested[40] = 1
+functionTested[41] = 1
+
+
+local function function_PLAYER_EVENT_ON_CHARACTER_CREATE(event, player)
+    functionTested[1] = 1
+    print('PLAYER_EVENT_ON_CHARACTER_CREATE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_CHARACTER_DELETE(event, guid)
+    functionTested[2] = 1
+    print('PLAYER_EVENT_ON_CHARACTER_DELETE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LOGIN(event, player)
+    functionTested[3] = 1
+    print('PLAYER_EVENT_ON_LOGIN has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LOGOUT(event, player)
+    functionTested[4] = 1
+    print('PLAYER_EVENT_ON_LOGOUT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_SPELL_CAST(event, player, spell, skipCheck)
+    functionTested[5] = 1
+    print('PLAYER_EVENT_ON_SPELL_CAST has fired')
+end
+
+local function function_PLAYER_EVENT_ON_KILL_PLAYER(event, killer, killed)
+    functionTested[6] = 1
+    print('PLAYER_EVENT_ON_KILL_PLAYER has fired')
+end
+
+local function function_PLAYER_EVENT_ON_KILL_CREATURE(event, killer, killed)
+    functionTested[7] = 1
+    print('PLAYER_EVENT_ON_KILL_CREATURE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_KILLED_BY_CREATURE(event, killer, killed)
+    functionTested[8] = 1
+    print('PLAYER_EVENT_ON_KILLED_BY_CREATURE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_DUEL_REQUEST(event, target, challenger)
+    functionTested[9] = 1
+    print('PLAYER_EVENT_ON_DUEL_REQUEST has fired')
+end
+
+local function function_PLAYER_EVENT_ON_DUEL_START(event, player1, player2)
+    functionTested[10] = 1
+    print('PLAYER_EVENT_ON_DUEL_START has fired')
+end
+
+local function function_PLAYER_EVENT_ON_DUEL_END(event, winner, loser, type)
+    functionTested[11] = 1
+    print('PLAYER_EVENT_ON_DUEL_END has fired')
+end
+
+local function function_PLAYER_EVENT_ON_GIVE_XP(event, player, amount, victim) -- Can return new XP amount
+    functionTested[12] = 1
+    print('PLAYER_EVENT_ON_GIVE_XP has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LEVEL_CHANGE(event, player, oldLevel)
+    functionTested[13] = 1
+    print('PLAYER_EVENT_ON_LEVEL_CHANGE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_MONEY_CHANGE(event, player, amount) -- Can return new money amount
+    functionTested[14] = 1
+    print('PLAYER_EVENT_ON_MONEY_CHANGE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_REPUTATION_CHANGE(event, player, factionId, standing, incremental) -- Can return new standing
+    functionTested[15] = 1
+    print('PLAYER_EVENT_ON_REPUTATION_CHANGE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_TALENTS_CHANGE(event, player, points)
+    functionTested[16] = 1
+    print('PLAYER_EVENT_ON_TALENTS_CHANGE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_TALENTS_RESET(event, player, noCost)
+    functionTested[17] = 1
+    print('PLAYER_EVENT_ON_TALENTS_RESET has fired')
+end
+
+local function function_PLAYER_EVENT_ON_CHAT(event, player, msg, Type, lang) -- Can return false, newMessage
+    functionTested[18] = 1
+    print('PLAYER_EVENT_ON_CHAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_WHISPER(event, player, msg, Type, lang, receiver) -- Can return false, newMessage
+    functionTested[19] = 1
+    print('PLAYER_EVENT_ON_WHISPER has fired')
+end
+
+local function function_PLAYER_EVENT_ON_GROUP_CHAT(event, player, msg, Type, lang, group) -- Can return false, newMessage
+    functionTested[20] = 1
+    print('PLAYER_EVENT_ON_GROUP_CHAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_GUILD_CHAT(event, player, msg, Type, lang, guild) -- Can return false, newMessage
+    functionTested[21] = 1
+    print('PLAYER_EVENT_ON_GUILD_CHAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_CHANNEL_CHAT(event, player, msg, Type, lang, channel) -- Can return false, newMessage
+    functionTested[22] = 1
+    print('PLAYER_EVENT_ON_CHANNEL_CHAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_EMOTE(event, player, emote) -- Not triggered on any known emote
+    functionTested[23] = 1
+    print('PLAYER_EVENT_ON_EMOTE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_TEXT_EMOTE(event, player, textEmote, emoteNum, guid)
+    functionTested[24] = 1
+    print('PLAYER_EVENT_ON_TEXT_EMOTE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_SAVE(event, player)
+    functionTested[25] = 1
+    print('PLAYER_EVENT_ON_SAVE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_BIND_TO_INSTANCE(event, player, difficulty, mapid, permanent)
+    functionTested[26] = 1
+    print('PLAYER_EVENT_ON_BIND_TO_INSTANCE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_UPDATE_ZONE(event, player, newZone, newArea)
+    functionTested[27] = 1
+    print('PLAYER_EVENT_ON_UPDATE_ZONE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_MAP_CHANGE(event, player)
+    functionTested[28] = 1
+    print('PLAYER_EVENT_ON_MAP_CHANGE has fired')
+end
+
+local function function_PLAYER_EVENT_ON_EQUIP(event, player, item, bag, slot)
+    functionTested[29] = 1
+    print('PLAYER_EVENT_ON_EQUIP has fired')
+end
+
+local function function_PLAYER_EVENT_ON_FIRST_LOGIN(event, player)
+    functionTested[30] = 1
+    print('PLAYER_EVENT_ON_FIRST_LOGIN has fired')
+end
+
+local function function_PLAYER_EVENT_ON_CAN_USE_ITEM(event, player, itemEntry) -- Can return InventoryResult enum value
+    functionTested[31] = 1
+    print('PLAYER_EVENT_ON_CAN_USE_ITEM has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LOOT_ITEM(event, player, item, count)
+    functionTested[32] = 1
+    print('PLAYER_EVENT_ON_LOOT_ITEM has fired')
+end
+
+local function function_PLAYER_EVENT_ON_ENTER_COMBAT(event, player, enemy)
+    functionTested[33] = 1
+    print('PLAYER_EVENT_ON_ENTER_COMBAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LEAVE_COMBAT(event, player)
+    functionTested[34] = 1
+    print('PLAYER_EVENT_ON_LEAVE_COMBAT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_REPOP(event, player)
+    print('PLAYER_EVENT_ON_REPOP has fired')
+    functionTested[35] = 1
+end
+
+local function function_PLAYER_EVENT_ON_RESURRECT(event, player)
+    functionTested[36] = 1
+    print('PLAYER_EVENT_ON_RESURRECT has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LOOT_MONEY(event, player, amount)
+    functionTested[37] = 1
+    print('PLAYER_EVENT_ON_LOOT_MONEY has fired')
+end
+
+local function function_PLAYER_EVENT_ON_QUEST_ABANDON(event, player, questId)
+    functionTested[38] = 1
+    print('PLAYER_EVENT_ON_QUEST_ABANDON has fired')
+end
+
+local function function_PLAYER_EVENT_ON_LEARN_TALENTS(event, player, talentId, talentRank, spellid)
+    functionTested[39] = 1
+    print('PLAYER_EVENT_ON_LEARN_TALENTS has fired')
+end
+
+local function function_PLAYER_EVENT_ON_COMMAND(event, player, command) -- player is nil if command used from console. Can return false
+    print('PLAYER_EVENT_ON_COMMAND has fired')
+    if command == "resetluatest" then
+        for n = 1,43,1 do
+            functionTested[n] = 0
+        end
+
+        --hardcoded marking as done what doesn't exist
+        functionTested[40] = 1
+        functionTested[41] = 1
+        functionTested[42] = 1
+        return false
+
+    elseif command == "luatest" then
+        local todoString = ''
+        for n = 1,43,1 do
+            if functionTested[n] == 0 then
+                if todoString == '' then
+                    todoString = 'Remaining player events: '..n
+                else
+                    todoString = todoString..', '..n
+                end
+            end
+        end
+
+        if player == nil then
+            print(todoString)
+        else
+            player:SendBroadcastMessage(todoString)
+        end
+        functionTested[42] = 1
+        return false
+    end
+
+    functionTested[42] = 1
+end
+
+local function function_PLAYER_EVENT_ON_PET_SPAWNED(event, player, pet)
+    print('PLAYER_EVENT_ON_PET_SPAWNED has fired')
+    functionTested[43] = 1
+end
+
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHARACTER_CREATE, function_PLAYER_EVENT_ON_CHARACTER_CREATE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHARACTER_DELETE, function_PLAYER_EVENT_ON_CHARACTER_DELETE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGIN, function_PLAYER_EVENT_ON_LOGIN)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOGOUT, function_PLAYER_EVENT_ON_LOGOUT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_SPELL_CAST, function_PLAYER_EVENT_ON_SPELL_CAST)
+RegisterPlayerEvent(PLAYER_EVENT_ON_KILL_PLAYER, function_PLAYER_EVENT_ON_KILL_PLAYER)
+RegisterPlayerEvent(PLAYER_EVENT_ON_KILL_CREATURE, function_PLAYER_EVENT_ON_KILL_CREATURE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_KILLED_BY_CREATURE, function_PLAYER_EVENT_ON_KILLED_BY_CREATURE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_DUEL_REQUEST, function_PLAYER_EVENT_ON_DUEL_REQUEST)
+RegisterPlayerEvent(PLAYER_EVENT_ON_DUEL_START, function_PLAYER_EVENT_ON_DUEL_START)
+RegisterPlayerEvent(PLAYER_EVENT_ON_DUEL_END, function_PLAYER_EVENT_ON_DUEL_END)
+RegisterPlayerEvent(PLAYER_EVENT_ON_GIVE_XP, function_PLAYER_EVENT_ON_GIVE_XP)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LEVEL_CHANGE, function_PLAYER_EVENT_ON_LEVEL_CHANGE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_MONEY_CHANGE, function_PLAYER_EVENT_ON_MONEY_CHANGE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_REPUTATION_CHANGE, function_PLAYER_EVENT_ON_REPUTATION_CHANGE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_TALENTS_CHANGE, function_PLAYER_EVENT_ON_TALENTS_CHANGE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_TALENTS_RESET, function_PLAYER_EVENT_ON_TALENTS_RESET)
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHAT, function_PLAYER_EVENT_ON_CHAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_WHISPER, function_PLAYER_EVENT_ON_WHISPER)
+RegisterPlayerEvent(PLAYER_EVENT_ON_GROUP_CHAT, function_PLAYER_EVENT_ON_GROUP_CHAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_GUILD_CHAT, function_PLAYER_EVENT_ON_GUILD_CHAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHANNEL_CHAT, function_PLAYER_EVENT_ON_CHANNEL_CHAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_EMOTE, function_PLAYER_EVENT_ON_EMOTE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_TEXT_EMOTE, function_PLAYER_EVENT_ON_TEXT_EMOTE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_SAVE, function_PLAYER_EVENT_ON_SAVE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_BIND_TO_INSTANCE, function_PLAYER_EVENT_ON_BIND_TO_INSTANCE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, function_PLAYER_EVENT_ON_UPDATE_ZONE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_MAP_CHANGE, function_PLAYER_EVENT_ON_MAP_CHANGE)
+RegisterPlayerEvent(PLAYER_EVENT_ON_EQUIP, function_PLAYER_EVENT_ON_EQUIP)
+RegisterPlayerEvent(PLAYER_EVENT_ON_FIRST_LOGIN, function_PLAYER_EVENT_ON_FIRST_LOGIN)
+RegisterPlayerEvent(PLAYER_EVENT_ON_CAN_USE_ITEM, function_PLAYER_EVENT_ON_CAN_USE_ITEM)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOOT_ITEM, function_PLAYER_EVENT_ON_LOOT_ITEM)
+RegisterPlayerEvent(PLAYER_EVENT_ON_ENTER_COMBAT, function_PLAYER_EVENT_ON_ENTER_COMBAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LEAVE_COMBAT, function_PLAYER_EVENT_ON_LEAVE_COMBAT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_REPOP, function_PLAYER_EVENT_ON_REPOP)
+RegisterPlayerEvent(PLAYER_EVENT_ON_RESURRECT, function_PLAYER_EVENT_ON_RESURRECT)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOOT_MONEY, function_PLAYER_EVENT_ON_LOOT_MONEY)
+RegisterPlayerEvent(PLAYER_EVENT_ON_QUEST_ABANDON, function_PLAYER_EVENT_ON_QUEST_ABANDON)
+RegisterPlayerEvent(PLAYER_EVENT_ON_LEARN_TALENTS, function_PLAYER_EVENT_ON_LEARN_TALENTS)
+RegisterPlayerEvent(PLAYER_EVENT_ON_COMMAND, function_PLAYER_EVENT_ON_COMMAND)
+RegisterPlayerEvent(PLAYER_EVENT_ON_PET_SPAWNED, function_PLAYER_EVENT_ON_PET_SPAWNED)
